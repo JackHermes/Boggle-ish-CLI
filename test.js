f3230038e575ab6cf7694961ba445fd556b0f953
Jordan Scholten <schojo13@gmail.com>
2017-03-05 13:17:28 -0800

    Add start command

--- test.js
+++ test.js
@@ -7,7 +7,6 @@ describe('Boggle', function() {
     it('should return a board with values between 1 and 9, inclusive', function() {
       boggle.integers.forEach(function(item) {
         assert.isAtMost(item, 9, 'generated number is less than or equal to 9');
-
       })
     });
   });
