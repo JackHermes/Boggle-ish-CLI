6a9fa595d7690450ec6b850ff04c92b5c00a2196
Jordan Scholten <schojo13@gmail.com>
2017-03-18 09:42:38 -0700

    Initial implementation

--- chainFinder.js
+++ chainFinder.js
@@ -1,34 +1,53 @@
-// declare traverser fn receiving a matrix as arg
-  // declare valid chains variable
-  // declare object to track visited cells
+let traverser = (matrix) => {
+  let validChains = '';
+  // object to track visited cells
+  let visited = {};
   // loop over matrix
-    //
-      // execute chainFinder fn, inputing this cell's coordinates and []
-      // mark cell as visited - marking here will make sure there won't be duplicates
-  // return valid chains variable
+  for(let i = 0; i < matrix.length; i++) {
+    for(let j = 0; j < matrix[i].length; j++) {
+      // finds all chains with a root of this cell
+      finder(j, i, []);
+      // mark cell as visited - marking here will make sure there won't be duplicate chains
+      let cell = '' + j + i;
+      visited[cell] = 1;
+    }
+  }
+  return validChains;
+};
 
 
+let finder = (x, y, workspace) => {
+  // check if x, y location has been a root/starting cell
+  // and propogate 'workspace'
+  if(visited['' + x + y]) return workspace;
 
-    // declare chainFinder fn receiving x, y and workspace args
-      // check if x, y location has been a root/starting cell
-        // if so, return (workspace?)
+  let current = matrix[x][y];
+  // declare workspace total as sum of wkspc items ----> TODO: make this an arg that's passed around - for complexities' sake!
+  let workspaceTotal = workspace.reduce((memo, value) => { return memo + value; }, 0)
 
-      // declare current as matrix at x, y
-      // declare workspace total as sum of wkspc items ----> TODO: make this an arg that's passed around for complexities' sake!
-
-      // check if x, y location is off board or if workspace sum + current is greater than area
-        // if so, return workspace
-      // check if workspace sum + current is less than area
-        // if so, push current to workspace
-          // if still less than area, recurse all 8 directions
-      // must be equal then
-        // Add current to workspace
-        // Recurse again, in case of a zero in our chain
-        // check if at least width - 1/ 2 integers in chain
-          // if so, push to valid chains variable (from external function)
-          // pop from workspace
-          // return workspace
-
+  // check if x, y location is off board or if workspace sum + current is greater than area
+  if(x < 0 || y < 0 || x >= 3 || y >= 3 || (workspaceTotal + current) > 9) {
+    // if so, propogate workspace
+    return workspace;
+  }
+  // check if workspace sum + current is less or equal to area
+  if((workspaceTotal + current) <= 9) {
+    // if so, push current to workspace
+    workspace.push(current);
+    // recurse all 8 directions (if already equal, we're checking for zeroes)
+    // check if equal
+    if((workspaceTotal + current) === 9) {
+      // if so, check if at least width - 1/ 2 integers in chain
+      if(workspace.length >= 2) {
+        // if so, push to valid chains variable (from external fn)
+        validChains.concat('\n' + workspace.join(' + ') + ' = 9');
+      }
+    }
+    // pop and return workspace to clear and propogate
+    workspace.pop();
+    return workspace;
+  }
+};
 
   /*
   I return workspace in many places in order to ensure I'm propogating the
